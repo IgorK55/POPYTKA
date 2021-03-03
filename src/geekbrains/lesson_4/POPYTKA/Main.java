42ffbe02e788f83670f6ca9ce6b386aab263ae80
IgorK55 <egor2008@mail.ru>
2021-03-03 15:45:46 +0200

    2-ой пробный запуск репозитория.

---
+++ src/geekbrains/lesson_4/POPYTKA/Main.java
@@ -0,0 +1,11 @@
+package geekbrains.lesson_4.POPYTKA;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Попытка исправить ситуацию");
+        System.out.println("Вроде бы всё получилось!");
+	// write your code here
+    }
+}
